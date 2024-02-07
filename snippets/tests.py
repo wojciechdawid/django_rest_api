d572a2c40d16ea849e35edd56b4fe6a03e5b1dda
Wojciech Dawid <wojciechdawid1@wp.pl>
2024-02-07 13:12:28 +0100

    unfinished tests

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from snippets.models import Snippet
 
-# Create your tests here.
+
+class TestSnippet(TestCase):
+
+    def test_save_highlighted(self):
+        s = Snippet.objects.create(code="x += 3")
+        self.assertEqual(s.highlighted, "")
+
+
+class TestSnippetView(TestCase):
+
+    def test_list(self):
+        user = "XXX"
+        Snippet.objects.create(code="x=1", owner=user)
+        response = self.client.get("/api/v1/snippets/", content_type="application/json")
+
+        self.assertEqual(response.json(), ["xxx"])
+
